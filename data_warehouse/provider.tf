65b80ccfca4e6952035f9daecd64a71a285b1a1b
장동하 <117894209+donghajang213@users.noreply.github.com>
2026-06-07 20:10:15 +0900

    Update provider.tf

--- data_warehouse/provider.tf
+++ data_warehouse/provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "google" {
-  project = "stock-data-pipeline-496004" # "여기에_GCP_프로젝트_ID를_넣어주세요"
+  project = "여기에_GCP_프로젝트_ID를_넣어주세요"
   region  = "asia-northeast3" # 서울 리전
 }
